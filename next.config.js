9424c50ea765859401267c856db600cda34dc584
abiizar <abiizar@Abiizar.lan>
2026-03-14 15:38:26 +0700

    Add rewrite: /v1/* -> /api/v1/*

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-}
+  async rewrites() {
+    return [
+      {
+        source: '/v1/:path*',
+        destination: '/api/v1/:path*',
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
